5a4020585cc1798b74e5c395ec9c4e2aa28f2051
Yana Barabashh <123150404+Yaname23@users.noreply.github.com>
2024-10-10 18:35:29 +0300

    Create HW_02_task02.py

---
+++ HW_02_task02.py
@@ -0,0 +1,13 @@
+num = int(input('ВВелите целое число: '))
+number = num
+hexadecimal_digits = "0123456789ABCDEF"
+hexadecimal_number = ""
+
+while number > 0:
+    remainder = number % 16
+    hexadecimal_digit = hexadecimal_digits[remainder]
+    hexadecimal_number = hexadecimal_digit + hexadecimal_number
+    number //= 16
+
+print(f'Шестнадцатеричное представление числа: {num}: 0x{hexadecimal_number}')
+print(f'Проверка результата:', hex(num))
