032e1086609ec0c84ae7752dc69c4a3ff9174b9a
Yana Barabashh <123150404+Yaname23@users.noreply.github.com>
2024-10-10 21:48:27 +0300

    Update Home_Work_07_task_03.py

--- Home_Work_07_task_03.py
+++ Home_Work_07_task_03.py
@@ -1,10 +1,6 @@
 """Из созданных на уроке и в рамках домашнего задания функций, соберите пакет для работы с файлами.
 
-Создайте файл __init__.py и запишите в него все функции:
-get_dir_size,
-save_results_to_json,
-save_results_to_csv,
-save_results_to_pickle, traverse_directory."""
+"""
 
 code_to_write = ''
 import os
